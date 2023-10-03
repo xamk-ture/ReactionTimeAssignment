f21e344acc04b8a6de52fe55d532248e846c6a0a
Tuomas Reijonen <hture01@xamk.fi>
2023-10-03 16:27:27 +0300

    Add project files.

---
+++ ReactionTimeAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+
+namespace ReactionTimeAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Luodaan uusi random-olio
+            Random random = new Random();
+            // Asettaa satunnaisen ajan 1-5 sekuntia.
+            int sleepTime = random.Next(1000, 5000); 
+            // Odottaa satunnaisen ajan 1-5 sekuntia.
+            Thread.Sleep(sleepTime);
+
+            // Aloittaa ajanoton.
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            //Lopettaa ajanoton
+            stopwatch.Stop();
+            //Otetaan talteen reaktioaika
+            double reactionTime = stopwatch.Elapsed.TotalMilliseconds;
+        }
+    }
+}
